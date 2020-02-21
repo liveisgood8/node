19ad6e53963196d3b4c411d6ef87d6bb419e7493
Лемешонок Кирилл <lemeshonok12@gmail.com>
2020-02-21 13:32:42 +0700

    node runner tool

---
+++ tools/runner/NodeRun/NodeRun.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+#include <windows.h>
+
+#include "node_runner.h"
+
+int main(int argc, char* argv[]) {
+  SetConsoleOutputCP(CP_UTF8);
+
+  SetEnvironmentVariableA("NODE_DISABLE_COLORS", "1");
+
+  RunNode(argc, const_cast<const char**>(argv));
+
+  //Runner::GetInstance()->Init(argc, const_cast<const char**>(argv));
+  //Runner::GetInstance()->RunScript("console.log('test')");
+}
