5129e3a81a2a3b16f56f7e4388590555209b4ffb
Лемешонок Кирилл <lemeshonok12@gmail.com>
2020-02-25 15:58:18 +0700

    NodeRun adopted for new interface

--- tools/runner/NodeRun/NodeRun.cpp
+++ tools/runner/NodeRun/NodeRun.cpp
@@ -1,16 +1,14 @@
 #include <iostream>
+#include <thread>
 
 #include <windows.h>
 
-#include "node_runner.h"
+#include "node_main.h"
 
 int main(int argc, char* argv[]) {
   SetConsoleOutputCP(CP_UTF8);
 
   SetEnvironmentVariableA("NODE_DISABLE_COLORS", "1");
 
-  RunNode(argc, const_cast<const char**>(argv));
-
-  //Runner::GetInstance()->Init(argc, const_cast<const char**>(argv));
-  //Runner::GetInstance()->RunScript("console.log('test')");
+  return RunNode(argc, const_cast<const char**>(argv));
 }
